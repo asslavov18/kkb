2907654a613b99a32255e6960b4f1a9dab6f9c1c
BDDimitrov18 <58163537+BDDimitrov18@users.noreply.github.com>
2020-10-20 22:18:03 +0300

    set up data

--- kkb/kkb/kkb.cpp
+++ kkb/kkb/kkb.cpp
@@ -1,14 +1,43 @@
-#include <iostream>
+#include <iostream> 
+#include <vector>
 using namespace std;
-void Menu()
+bool Menu()
 {
-
+	vector<vector<int>> a;
+	vector<int> c;
+	int n;
+	int num;
+	cout << "Enter the number of arrays";
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		while (cin.get() != '\n')
+		{
+			cin >> num;
+			c.push_back(num);
+		}
+		a.push_back(c);
+		c.erase(c.begin(),c.end());
+	}
+	for (int i = 0;i < a.size(); i++)
+	{
+		for (int j = 0; j < a[i].size(); j++)
+		{
+			cout << a[i][j] << " ";
+		}
+		cout << endl;
+	}
+	/*
+	1.obedinenie
+	2.sechenie
+	3.razlika
+	4.simetrichna razlika
+	5.Podmnojestvo
+	*/
+	return true;
 }
-void Input()
-{
 
-}
 int main()
 {
-
+	while (Menu());
 }
