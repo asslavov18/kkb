71c624ce91fb2a87797c20b95ee11544c092d4a9
BDDimitrov18 <58163537+BDDimitrov18@users.noreply.github.com>
2020-10-25 21:54:01 +0200

    input system changes

--- kkb/kkb/kkb.cpp
+++ kkb/kkb/kkb.cpp
@@ -1,39 +1,44 @@
 #include <iostream> 
 #include <vector>
+#include <string>
+
 using namespace std;
+int stringToNumber(string numTxt) {
+	// will do later
+	return 0;
+}
+
+void divideStrings(string line) {
+	vector<int> numsArr;
+	while (line.find(" ") != string::npos)
+	{
+		cout << line.substr(0, line.find(" ")) << endl;
+		stringToNumber(line.substr(0, line.find(" ")));
+		line = line.substr(line.find(" ") + 1, line.size() - line.find(" ") + 1);
+	}
+	cout << line;
+	stringToNumber(line);
+}
+
+
+
 bool Menu()
 {
-	vector<vector<int>> a;
-	vector<int> c;
-	int n;
-	int num;
-	cout << "Enter the number of arrays";
-	cin >> n;
-	for (int i = 0; i < n; i++)
+	vector<string> lines;
+	string line;
+	int arrays;
+	cin >> arrays;
+	cin.ignore();
+	for (int i = 0; i < arrays; i++)
 	{
-		while (cin.get() != '\n')
-		{
-			cin >> num;
-			c.push_back(num);
-		}
-		a.push_back(c);
-		c.erase(c.begin(),c.end());
+		getline(cin, line);
+		lines.push_back(line);
 	}
-	for (int i = 0;i < a.size(); i++)
+	for (int i = 0; i < arrays; i++)
 	{
-		for (int j = 0; j < a[i].size(); j++)
-		{
-			cout << a[i][j] << " ";
-		}
-		cout << endl;
+		cout << lines[i] << endl;
 	}
-	/*
-	1.obedinenie
-	2.sechenie
-	3.razlika
-	4.simetrichna razlika
-	5.Podmnojestvo
-	*/
+	divideStrings(lines[0]);
 	return true;
 }
 
